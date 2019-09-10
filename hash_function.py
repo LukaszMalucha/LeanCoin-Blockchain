af72c40421189b7fff827fee0d97fbd3da98dd2d
unknown <lucasmalucha@gmail.com>
2019-09-10 20:29:27 +0100

    data

--- hash_function.py
+++ hash_function.py
@@ -1,19 +1,51 @@
 from cryptography.hazmat.backends import default_backend
 from cryptography.hazmat.primitives import hashes
 
-digest = hashes.Hash(hashes.SHA256(), backend=default_backend()) # init hash
-digest.update(b"abc") # add some data
-digest.update(b"123") # add some more data
-hash = digest.finalize() # close hash
-
-print(hash)
-
-
-digest_2 = hashes.Hash(hashes.SHA256(), backend=default_backend()) # init hash
-digest_2.update(b"abasd") # add some data
-digest_2.update(b"1234") # add some more data
-hash2 = digest_2.finalize() # close hash
-
-print(hash2)
 
 # https://cryptography.io/en/latest/hazmat/primitives/cryptographic-hashes/
+
+class someClass:
+    string = None
+    def __init__(self, mystring):
+        self.string = mystring
+    def __repr__(self):
+        return self.string
+
+
+
+class CBlock:
+    data = None
+    previousHash = None
+    previousBlock = None
+    def __init__(self, data, previousBlock):
+        self.data = data
+        self.previousBlock = previousBlock
+        if previousBlock != None:
+            self.previousHash = previousBlock.computeHash()
+    def computeHash(self):
+        digest = hashes.Hash(hashes.SHA256(), backend=default_backend())  # init hash
+        digest.update(bytes(str(self.data), 'utf8'))  # hash some data
+        digest.update(bytes(str(self.previousHash), 'utf8'))  # hash previous block hash as well
+        return digest.finalize()  # close hash
+
+
+if __name__ == '__main__':
+    root = CBlock('I am root', None)
+    B1 = CBlock('I am a child.', root)
+    B2 = CBlock('I am B1s brother', root)
+    B3 = CBlock(12345, B1)
+    B4 = CBlock(someClass('Hi there!'), B2)
+    B5 = CBlock("Top block", B4)
+
+    for b in [B1, B2, B3, B4, B5]:
+        if B1.previousBlock.computeHash() == B1.previousHash:
+            preint("Success! Hash is good")
+        else:
+            print("ERROR! Hash is not good")
+
+    # change data without changing hash and check if blockchain detects tampering
+    B3.data = 12345
+    if B4.previousBlock.computeHash() == B4.previousHash:
+        preint("ERROR! Tampering not detected")
+    else:
+        print("GOOD! Tampering detected")
